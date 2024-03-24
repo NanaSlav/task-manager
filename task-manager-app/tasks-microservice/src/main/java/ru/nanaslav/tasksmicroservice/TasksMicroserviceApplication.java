e04f21bcd02c39cdb464a5e8092f4997092a5298
nanaSlav <slavina270@gmail.com>
2024-03-24 17:48:05 +0300

    #3 Добавлен шаблон для микросервиса задач

---
+++ task-manager-app/tasks-microservice/src/main/java/ru/nanaslav/tasksmicroservice/TasksMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.nanaslav.tasksmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksMicroserviceApplication.class, args);
+    }
+
+}
