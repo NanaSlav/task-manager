f9a354c44d85613fcecbec73c95b8454ba4440e3
nanaSlav <slavina270@gmail.com>
2024-04-27 23:10:22 +0300

    #9 Базовые настройки для spring security

--- task-manager-app/users-microservice/src/main/java/ru/nanaslav/usersmicroservice/config/MongoConfig.java
+++ task-manager-app/users-microservice/src/main/java/ru/nanaslav/usersmicroservice/config/MongoConfig.java
@@ -24,7 +24,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27018/users");
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/users");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
---
+++ task-manager-app/users-microservice/src/main/java/ru/nanaslav/usersmicroservice/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+/**
+ * Created on 27/04/2024
+ */
+package ru.nanaslav.usersmicroservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.nanaslav.usersmicroservice.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Autowired
+    UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
