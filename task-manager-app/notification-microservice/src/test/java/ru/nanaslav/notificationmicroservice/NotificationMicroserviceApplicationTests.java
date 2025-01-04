c5db8bb295192fa2b6d804589520b7edcfbfd618
NanaSlav <slavina270@gmail.com>
2025-01-04 22:41:17 +0300

    Создан микросервис уведомлений

---
+++ task-manager-app/notification-microservice/src/test/java/ru/nanaslav/notificationmicroservice/NotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nanaslav.notificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
